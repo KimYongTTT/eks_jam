a4df074c5b17501a744ef6056496cb02e20319d9
Your Name <you@example.com>
2024-03-27 09:09:06 +0000

    external alb & tg 생성중

--- eks-blue/main.tf
+++ eks-blue/main.tf
@@ -14,3 +14,12 @@ module "eks-blue" {
 
   vpc_id = data.aws_vpc.vpc.id
 }
+
+
+#TODO lb --> ingress tg 생성
+# resource "aws_lb_target_group" "tg-blue" {
+#   name = "tg-blue-ui"
+#   port = 80
+#   protocol = "HTTP"
+#   vpc_id = data.aws_vpc.vpc.id
+# }
