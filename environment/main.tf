de3152ff10a66c2a1fd5c9b5f9e1714dd11daa84
KimYongTTT <yongtae0104@gmail.com>
2024-04-22 15:31:22 +0900

    codecommit delete

--- environment/main.tf
+++ environment/main.tf
@@ -84,8 +84,3 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
-
-resource "aws_codecommit_repository" "gitops" {
-  repository_name = "eks-jam-gitops-repository"
-  description     = "CodeCommit repository for GitOps"
-}
